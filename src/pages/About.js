be8a8a695e17a4b536eb2a7c8dd569a8e80417f2
FBIV7 <71649173+FBIV7@users.noreply.github.com>
2021-03-03 16:13:48 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,27 +18,19 @@ const About = () => {
         </div>
         <div className="about text-center m-22 col-span-3">
           <div className="about-text">
-            <p>
-              VFAST is a fastest evolving verification product by FBIV Infocomm
-              Pvt Ltd in India. We provide a compliant & robust background
-              verification process and specialize in helping organizations of
-              all sizes and locations efficiently implement, utilize and refine
-              their background screening programs. We mitigate risk that comes
-              with hiring as we equip our clients for making the best hire
-              decisions and help them build successful relationships with their
-              applicants based on trust and safety.
-            </p>
-            <p></p>
+
             <div className="text-center m-4 ">
-              <h3>Why Choose Us?</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul className="aboutLi">
-                    <li className="fontServ">TRUST</li>
-                    <li className="fontServ">TECHNOLOGY</li>
-                    <li className="fontServ">EVIDENCE</li>
-                    <li className="fontServ"> RELIABILITY</li>
+                  <ul className='aboutLi'>
+                    <li className='fontServ'>TRUST</li>
+                    <p> We use a combination of human and machine-based resources to build trust among our customers. </p>
+                    <li className='fontServ'>TECHNOLOGY</li>
+                    <p>Our verification process is totally technology based, we use The latest technologies for the complete process and generate an authentic report</p>
+                    <li className='fontServ'>EVIDENCE</li>
+                    <p>In our system, you can access all the supporting documents collected during the course of screening as per your requirement and convenience. </p>
                   </ul>
+                  {/* <p>Our NAPBS-accredited  team spans over 200 countries and territories. We provide over 150 unique background check services for your specific needs globally.</p> */}
                 </div>
               </div>
             </div>
