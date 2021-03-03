b342515d44b5ebe4c1849f27c424601e8cdaa6ab
FBIV7 <71649173+FBIV7@users.noreply.github.com>
2021-03-03 16:19:53 +0530

    Update Membership.js

--- src/pages/Membership.js
+++ src/pages/Membership.js
@@ -3,16 +3,16 @@ import React from "react";
 const Membership = () => {
     return (
         <div>
-            <div className="row footer-logos partners p-4" style={{ backgroundColor: "#efefef" }}>
-                <div className="flex justify-center text-2xl section-title font-serif md:text-5xl" >
+            <div className="row footer-logos partners p-2" style={{ backgroundColor: "#efefef" }}>
+                {/* <div className="flex justify-center text-2xl section-title font-serif md:text-5xl" >
                     <div
                         className=" contactFont flex justify-center "
                     // style={{ marginTop: "50px" }}
                     >
                         BUSINESS PARTNER
           </div>
-                </div>
-                <div className="flex justify-around">
+                </div> */}
+                {/* <div className="flex justify-around">
                     <img
                         className="styleMembership"
                         src="../assets/footer/Yes_Bank.png"
@@ -25,10 +25,10 @@ const Membership = () => {
                     <img className="styleMembership" src="../assets/footer/hdb.jpg" />
                     <img className="styleMembership" src="../assets/footer/hdfc.png" />
                     <img className="styleMembership" src="../assets/footer/STFC.png" />
-                </div>
+                </div> */}
             </div>
-            <br></br>
-            <div className="row footer-logos partners p-4" style={{ backgroundColor: "#efefef" }}>
+            {/* <br></br> */}
+            <div className="row footer-logos partners p-2" style={{ backgroundColor: "#efefef" }}>
                 <div className="flex justify-center text-2xl section-title font-serif md:text-5xl">
                     <div
                         className=" contactFont flex justify-center "
